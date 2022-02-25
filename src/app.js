df289dad266050c7bddde8dfb15e049e5453c773
c18742005 <c18742005@mytudublin.ie>
2022-02-25 15:00:49 +0000

    Removed check on staff role

--- src/app.js
+++ src/app.js
@@ -13,6 +13,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.json({ type: 'application/vnd.api+json' }));
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(index);
 app.use('/api/', clientRoute);
